6c15d4ef631ada1d0309d22cb8b3119e0a9c0dae
Manuela Köpfli <koepflma@uw.edu>
2024-02-27 15:11:21 -0800

    scatseisnet works

--- scatseisnet/elbow.py
+++ scatseisnet/elbow.py
@@ -1,62 +1,43 @@
+"""
+This file calculated the distance between the time windows and their cluster center.
+The output can be used to create the elbow curve.
+The input is provided by scaternet.py
+"""
 import os
-import io
 import sys
-import pickle
-
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-import datetime
-
 import numpy as np
-import pandas as pd
-import obspy
-from obspy.clients.fdsn.client import Client
-client = Client('IRIS')
 import sklearn
-from sklearn.decomposition import FastICA
 from sklearn.cluster import KMeans
 
-from scatseisnet import ScatteringNetwork
+# parameter ========================================================================================================
 
-# model (octave, resolution,quality)
-str_network = 'o4_r4_q1_o5_r2_q2'
-# duration
-duration = '2month'
-# month = ''
-# seismic station
-str_station = 'multistation'
-
-# path to load and save averything exept of figures
-dirpath_load = '/data/wsd03/data_manuela/MtStHelens/scatseisnet/{}/{}/{}/'.format(str_network,duration,str_station)
-
-
-# # path to load and save everything
-# dirpath = '../data/scatseisnet/'
+n_clusters = 20 # define the maximum number of cluster to test
 
 # path to save figures
-dirpath_save = '../output/data/'
+dirpath = '../output/data/'
 # bild path to save figures if it does not exist yet
-os.makedirs(dirpath_save, exist_ok=True)
+os.makedirs(dirpath, exist_ok=True)
 
 # Load features
-features = np.load(dirpath_load+"/independent_components.npz", allow_pickle=True)['features']
-times = np.load(dirpath_load+"/independent_components.npz", allow_pickle=True)['times']
+features = np.load(dirpath+"independent_components.npz", allow_pickle=True)['features']
+times = np.load(dirpath+"independent_components.npz", allow_pickle=True)['times']
 print('features shape: {}\ntimes shape: {}'.format(features.shape,times.shape))
 
-#-----------------------------------------------------------------------------------------------------------------------------
-n_clusters = 20
+# clustering for different numbers of clusters =======================================================================
+
 sse = np.zeros(n_clusters)
 
 for N_CLUSTERS in range(1,n_clusters+1):
 
-    sys.stdout.write('\r{} of {}'.format(N_CLUSTERS, n_clusters+1))
+    sys.stdout.write('\r{} of {}'.format(N_CLUSTERS+1, n_clusters+1))
     sys.stdout.flush()
     
     # Perform clustering
     model_cluster = KMeans(n_clusters=N_CLUSTERS, n_init='auto', random_state=4)
     model_cluster.fit(features)
     
-    sse[N_CLUSTERS-1] = model_cluster.inertia_ #Sum of squared distances of samples to their closest cluster center, weighted by the sample weights if provided
+    #Sum of squared distances of samples to their closest cluster center, weighted by the sample weights if provided
+    sse[N_CLUSTERS-1] = model_cluster.inertia_ 
 
-np.savez(dirpath_save+"sse.npz",sse=sse)
+np.savez(dirpath+"sse.npz",sse=sse)
 print('\n***DONE***')
--- scatseisnet/scatenet.py
+++ scatseisnet/scatenet.py
@@ -1,5 +1,6 @@
-# Scatering Network for Seismology
-
+"""
+Scatering Network for Seismology (scatseisnet)
+"""
 import os
 import pickle
 import matplotlib.dates as mdates
@@ -12,22 +13,38 @@ import sys
 import io
 from scatseisnet import ScatteringNetwork
 
-s1  = 'UW-EDM-EHZ'
+# parameter ========================================================================================================
+s1  = 'UW-EDM-EHZ' # network-station-channel
 s2  = 'UW-SHW-EHZ'
 s3  = 'UW-HSR-EHZ'
 
 list_stations = [s1,s2,s3] # make a list of all stations
+
+s_time = obspy.UTCDateTime("2004-09-01T00:00:00")
+e_time=obspy.UTCDateTime("2004-09-03T00:00:00.1")
+
+resample_rate = 50 # Hz
+hp_filter = 1.0 # Hz
+
+segment_duration_seconds = 60.0
+overlap = 0.5 # betwen 0 and 1
+samples_per_segment = int(segment_duration_seconds * resample_rate)
+bank_keyword_arguments = (
+    {"octaves": 4, "resolution": 4, "quality": 1},
+    {"octaves": 5, "resolution": 2, "quality": 2},
+)
   
 # path to save figures
-dirpath_save = '../output/data/'
+dirpath = '../output/data/'
 # bild path to save figures if it does not exist yet
-os.makedirs(dirpath_save, exist_ok=True)
+os.makedirs(dirpath, exist_ok=True)
 
 print('Package imported and folder created.')
 
 stime = time.time() # start time
 
 # load data ========================================================================================================
+
 client = Client("IRIS")
 
 stream = obspy.Stream()
@@ -35,7 +52,6 @@ list_netstacha = []
 
 for xx in list_stations:
     # Collect waveforms from the datacenter
-    
     netstacha = xx.split('-')
     net = netstacha[0] # network
     sta = netstacha[1] # station
@@ -47,55 +63,46 @@ for xx in list_stations:
             station=sta,
             location="*",
             channel=cha,
-            starttime=obspy.UTCDateTime("2004-09-01T00:00:00"), #"2004-09-01T00:00"
-            endtime=obspy.UTCDateTime("2004-09-03T00:00:00.1"), #"2004-11-01T00:00.1"
+            starttime=s_time,
+            endtime=e_time,
         )
-#         st.trim(obspy.UTCDateTime("1980-03-31T00:00"),obspy.UTCDateTime("1980-05-19T00:00:00.1"),pad=True, fill_value=0)
-
+        # pre-processing
         st.detrend("linear")
         st.taper(0.05, type='hann')
-        st.resample(50)
+        st.resample(resample_rate)
         st.merge(fill_value=0)
-
-        st.filter(type="highpass", freq=1.0)
+        st.filter(type="highpass", freq=hp_filter)
+        
+        # append seismic-data and station-data
         stream += st
         list_netstacha.append(xx)
         print('{}'.format(netstacha))
     except:
         print('no data for {}'.format(netstacha))
         pass
-    
+
+# save stream if not empty
 if len(stream) == 0:
     print('empty stream')
     sys.exit()
 if len(stream) != 0:
-    stream.write(dirpath_save+"scattering_stream.mseed", format="MSEED")
+    stream.write(dirpath+"scattering_stream.mseed", format="MSEED")
     print(stream)
 
 print('Loading data tooks {} minutes.'.format(round((time.time()-stime)/60,3)))
-
-# create layers ======================================================================================================
-
-segment_duration_seconds = 60.0
-sampling_rate_hertz = stream[0].stats.sampling_rate
-samples_per_segment = int(segment_duration_seconds * sampling_rate_hertz)
-bank_keyword_arguments = (
-    {"octaves": 4, "resolution": 4, "quality": 1},
-    {"octaves": 5, "resolution": 2, "quality": 2},
-)
 
 # create scatnet ======================================================================================================
 
 network = ScatteringNetwork(
     *bank_keyword_arguments,
     bins=samples_per_segment,
-    sampling_rate=sampling_rate_hertz,
+    sampling_rate=resample_rate,
 )
 
 print(network)
 
 # Save the scattering network with Pickle
-filepath_save = os.path.join(dirpath_save, "scattering_network.pickle")
+filepath_save = os.path.join(dirpath, "scattering_network.pickle")
 with open(filepath_save, "wb") as file_save:
     pickle.dump(network, file_save, protocol=pickle.HIGHEST_PROTOCOL)
 
@@ -103,7 +110,6 @@ with open(filepath_save, "wb") as file_save:
 
 # Extract segment length (from any layer)
 segment_duration = network.bins / network.sampling_rate
-overlap = 0.5
 
 # Gather list for timestamps and segments
 timestamps = list()
@@ -115,16 +121,16 @@ for traces in stream.slide(segment_duration, segment_duration * overlap):
     segments.append(np.array([trace.data[:-1] for trace in traces]))
     
 # scattering transform ===============================================================================================
+
 st = time.time() # start time
 scattering_coefficients = network.transform(segments, reduce_type=np.mean)
 
 # Save the features in a pickle file
 np.savez(
-    dirpath_save+"scattering_coefficients.npz",
+    dirpath+"scattering_coefficients.npz",
     order_1=scattering_coefficients[0],
     order_2=scattering_coefficients[1],
     times=timestamps,
-#     list_netstacha=list_stations
     list_netstacha=list_netstacha
 )
 print('Scattering transform and saving took {} hours.'.format(round((time.time()-st)/3600,3)))
